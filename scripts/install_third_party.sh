001678b638e532449f679ea49489b870ccfaca9b
JudyYe <yeyf13.judy@gmail.com>
2023-03-02 16:59:20 -0500

    new hoi4d

--- scripts/install_third_party.sh
+++ scripts/install_third_party.sh
@@ -17,7 +17,7 @@ pip install "git+https://github.com/hassony2/manopth.git"
 # clone the repo in order to access pre-defined configs in PointRend project
 cd externals
 git clone --branch v0.6 https://github.com/facebookresearch/detectron2.git detectron2
-# install detectron2 from source
-pip install -e detectron2
-cd ../
-# See https://detectron2.readthedocs.io/tutorials/install.html for other installation options
+# # install detectron2 from source
+# pip install -e detectron2
+# cd ../
+# # See https://detectron2.readthedocs.io/tutorials/install.html for other installation options
